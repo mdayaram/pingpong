b4b1bfc973340a37b92ba746b53045ef1a725238
Manoj Dayaram <noj@moovweb.com>
2012-09-17 01:37:51 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingpong::Application.config.secret_token = '0d184935f0a516614e8b9f8960ef949fed23eb1b342911d940633c6a0cf8c7e18bef35f431e4c88082bcbee8e8757a9520670cab676dd64781dbc94cc3fd54e9'
