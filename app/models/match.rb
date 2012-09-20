d7ceed45da0e1c12cab9a600be335f05fd1e8afc
Manoj Dayaram <noj@moovweb.com>
2012-09-20 00:10:11 -0700

    Making the bracket views show team names and stuff

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,10 +1,13 @@
 class Match < ActiveRecord::Base
-  attr_accessible :next, :round, :team1, :team2, :winner, :schedule, :bracket
-  has_one :left_team, :class_name => "Team", :foreign_key => "team1"
-  has_one :right_team, :class_name => "Team", :foreign_key => "team2"
-  has_one :win_team, :class_name => "Team", :foreign_key => "winner"
-  has_one :next_match, :class_name => "Match", :foreign_key => "next"
-  has_one :next_loser_match, :class_name => "Match", :foreign_key => "loser_next"
+  attr_accessible :next, :loser_next
+  attr_accessible :team1, :team2, :winner
+  attr_accessible :round, :schedule, :bracket
+
+  belongs_to :left_team, :class_name => "Team", :foreign_key => "team1"
+  belongs_to :right_team, :class_name => "Team", :foreign_key => "team2"
+  belongs_to :win_team, :class_name => "Team", :foreign_key => "winner"
+  belongs_to :next_winner_match, :class_name => "Match", :foreign_key => "next"
+  belongs_to :next_loser_match, :class_name => "Match", :foreign_key => "loser_next"
 
   validates :bracket, :inclusion => { 
     :in => %w(serious serious_wildcard fun fun_wildcard doubles doubles_wildcard), 
