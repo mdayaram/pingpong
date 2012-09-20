454fffb219c3cfec2fd66db66ed76ef0dc5b671e
Manoj Dayaram <noj@moovweb.com>
2012-09-20 02:06:21 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920074314) do
+ActiveRecord::Schema.define(:version => 20120920090333) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(:version => 20120920074314) do
     t.integer  "winner"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.datetime "schedule"
     t.string   "bracket"
     t.integer  "loser_next"
+    t.datetime "schedule"
   end
 
   create_table "teams", :force => true do |t|
