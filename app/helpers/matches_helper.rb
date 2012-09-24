5699466ad629673059097a9d49b7fd209016552e
Manoj Dayaram <noj@moovweb.com>
2012-09-23 20:54:23 -0700

    something's weird

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,8 +1,8 @@
 module MatchesHelper
 
   def bracket_match(match, left_default, right_default)
-    left_team = match.left_team.nil? ? left_default : match.left_team.name
-    right_team = match.right_team.nil? ? right_default : match.right_team.name
+    left_team = match.left_team.blank? ? left_default : match.left_team.name
+    right_team = match.right_team.blank? ? right_default : match.right_team.name
     link_to "#{left_team} v #{right_team}", match
   end
 end
